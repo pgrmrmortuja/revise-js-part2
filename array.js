02e4a34dd8ec267f34cdba8af9764fe7c886c49d
pgrmrmortuja <moutasimmortuja@gmail.com>
2026-08-24 23:31:27 +0600

    array, push, pop, unshift and shift mehod of array

--- array.js
+++ array.js
@@ -1,2 +1,20 @@
 let x = [1,2,3];
+
+//add to last
+x.push(4);
+
+//remove from last
+x.pop(4);
+
+//add to first
+x.unshift(5);
+
+//remove from first
+x.shift(5);
+
 console.log(x);
+
+
+//string array
+let name = ["Mortuja", "Reyad"];
+console.log(name[1]);
